5bab83b3811cdf53141fa0e92a0cae08a78343e8
Fattakhova <ilnuro4ka01@gmail.com>
2021-10-18 21:33:31 +0500

    cors added

--- app.js
+++ app.js
@@ -23,6 +23,31 @@ mongoose.connect(DATA_BASE, {
   useUnifiedTopology: true,
 });
 
+const allowedCors = [
+  'https://movies-diploma.nomoredomains.rocks/',
+  'https://api.movies-diploma.nomoredomains.rocks/',
+  'http://localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.end();
+  }
+
+  next();
+});
+
 app.use(helmet());
 
 app.use(requestLogger);
