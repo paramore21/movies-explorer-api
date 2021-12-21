fd354a86a9460242b942c025cf6a07a230067895
Fattakhova <ilnuro4ka01@gmail.com>
2021-12-22 01:10:04 +0500

    fixed required email in patch users/me

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const { getUser, updateUser } = require('../controllers/users');
-const { updateUserValidation } = require('../middlewares/validate');
 
 router.get('/me', getUser);
-
-router.patch('/me', updateUserValidation, updateUser);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), updateUser);
 
 module.exports = router;
